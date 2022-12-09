44b7a70fc72858923950535c93b939e2a1748583
Pappycute <hassantesleem52@gmail.com>
2022-12-09 03:12:51 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always 0 (Success)
 */
-main(void)
 int main(void)
 {
 	printf("Size of a char : %zu byte(s)\n", sizeof(char));
